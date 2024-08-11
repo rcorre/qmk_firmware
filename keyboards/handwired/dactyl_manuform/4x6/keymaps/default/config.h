0702f7c30ed56365fc8aa099574c2a867ad853c3
Ryan Roden-Corrent <ryan@rcorre.net>
2024-08-11 11:49:47 -0400

    Debug dactyl

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/default/config.h
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/default/config.h
@@ -21,7 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 /* Select hand configuration */
-//#define MASTER_LEFT
-//#define MASTER_RIGHT
+#define MASTER_LEFT
 
-#define EE_HANDS
+// #define EE_HANDS
