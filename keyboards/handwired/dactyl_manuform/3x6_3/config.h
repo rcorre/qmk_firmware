b1398664e7699fac20ad1a50676bef59d87b9c28
Ryan Roden-Corrent <ryan@rcorre.net>
2024-09-09 17:58:27 -0400

    Clean up config.h

--- keyboards/handwired/dactyl_manuform/3x6_3/config.h
+++ keyboards/handwired/dactyl_manuform/3x6_3/config.h
@@ -6,7 +6,7 @@
 #define SERIAL_USART_TX_PIN GP0
 #define SERIAL_USART_RX_PIN GP1
 #define SERIAL_PIO_USE_PIO0
-#define SERIAL_USART_TIMEOUT 100 // USART driver timeout. default 100
+#define SERIAL_USART_TIMEOUT 100
 #define SERIAL_USART_SPEED 921600
 
 // D+/D- are wired to GP0/GP1 on the left half, and reversed on the right.
@@ -19,5 +19,5 @@
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED_MASK 0U
 
-// Enables This makes it easier for fast typists to use dual-function keys
+// This makes it easier for fast typists to use dual-function keys
 #define PERMISSIVE_HOLD
