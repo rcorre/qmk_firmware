581311bd685bd62e2e8e8dd19f7e5e51bd61c074
Ryan Roden-Corrent <ryan.roden-corrent@tanium.com>
2024-11-19 08:34:56 -0500

    dactyl: fix nav keys

--- keyboards/handwired/dactyl_manuform/3x6_3/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/3x6_3/keymaps/default/keymap.c
@@ -36,8 +36,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // Nav
     [2] = LAYOUT_split_3x6_3(
-         _______, KC_WH_U, KC_MS_U, KC_WH_D,    _______,    _______,                           _______, _______, _______, _______, _______, QK_BOOT,
-         _______, KC_MS_L, KC_MS_D, KC_MS_R,    _______,    _______,                           _______, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, _______,
+         _______, KC_WH_D, KC_MS_U, KC_WH_U,    _______,    _______,                           _______, _______, _______, _______, _______, QK_BOOT,
+         _______, KC_MS_L, KC_MS_D, KC_MS_R,    _______,    KC_DEL ,                           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
          _______, _______, _______, _______,    _______,    _______,                           _______, _______, _______, _______, _______, _______,
                                     KC_MS_BTN1, KC_MS_BTN2, _______,                           _______, _______, _______
     ),
